419a278648591782ba79f5a83b137467667b1197
Promisekel <101825563+Promisekel@users.noreply.github.com>
2024-11-06 11:27:02 +0000

    Update model.py

--- app/model.py
+++ app/model.py
@@ -9,20 +9,19 @@ pathlib.WindowsPath = pathlib.PosixPath
 def load_model():
     model_path = 'saved_model/pneumonia_classification_model.pkl'
     
-    Ensure the path is a string
+    # Ensure the path is a string
     model_path_str = str(model_path)
     
-    Debugging: Print the path to ensure it's correct
+    # Debugging: Print the path to ensure it's correct
     print(f"Loading model from: {model_path_str}")
     
-     Load the model
+    # Load the model
     model = load_learner(model_path_str)
     
-     Return the model to you
+    # Return the model
     return model
 
 def load_xray_detector():
     xray_model_path = 'saved_model/xray_detector_model.pkl'
     xray_model = load_learner(xray_model_path)
     return xray_model
-
