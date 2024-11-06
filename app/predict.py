445036bf50380267388b2d252cd51d78dea156c6
Promisekel <101825563+Promisekel@users.noreply.github.com>
2024-11-06 13:27:10 +0000

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -10,6 +10,5 @@ def is_xray(model, img_path):
     img = PILImage.create(img_path)
     pred_class, pred_idx, probs = model.predict(img)
     # Check if the predicted class is in the Xray classes
-    xray_classes = ["PATIENT IS PRESENTED WITH PNEUMONIA", "NORMAL; PATEINT HAS NO PNEUMONIA"]
+    xray_classes = ["PNEUMONIA", "NORMAL"]
     return pred_class in xray_classes
-
