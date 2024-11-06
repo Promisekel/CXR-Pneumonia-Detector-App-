7c9dc59f3f9654b116714fec0eb0370b23ffa427
Promisekel <101825563+Promisekel@users.noreply.github.com>
2024-11-06 11:01:38 +0000

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -1,31 +1,16 @@
-#from fastai.vision.all import PILImage
-
-#def predict(model, img_path):
- #   img = PILImage.create(img_path)
-  #  pred_class, pred_idx, probs = model.predict(img)
-   # return pred_class
-
-
-#def is_xray(model, img_path):
- #   img = PILImage.create(img_path)
-  #  pred_class, pred_idx, probs = model.predict(img)
-    # Check if the predicted class is in the Xray classes
-    #xray_classes = ["PNEUMONIA", "NORMAL"]
-    #return pred_class in xray_classes
-
-
 from fastai.vision.all import PILImage
 
 def predict(model, img_path):
-    # Convert img_path to string to ensure compatibility with PILImage
-    img = PILImage.create(str(img_path))
+    img = PILImage.create(img_path)
     pred_class, pred_idx, probs = model.predict(img)
     return pred_class
 
+
 def is_xray(model, img_path):
-    # Convert img_path to string to ensure compatibility with PILImage
-    img = PILImage.create(str(img_path))
+    img = PILImage.create(img_path)
     pred_class, pred_idx, probs = model.predict(img)
-    # Check if the predicted class is in the X-ray classes
+    Check if the predicted class is in the Xray classes
     xray_classes = ["PNEUMONIA", "NORMAL"]
     return pred_class in xray_classes
+
+
