54bba23a62b6de964ed513468df1983c3eb70595
Promisekel <101825563+Promisekel@users.noreply.github.com>
2024-11-07 09:27:19 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -107,8 +107,13 @@ if image_files:
             st.write("sCAN IS AN X-RAY. Scanning for pneumonia...")
             label = predict(model, image_path)
             st.write(f"Outcome of scan: {label}")
-            tick_icon = Image.open(tick_icon_path)
-            st.image(tick_icon, caption=" ", width=20)  # Display tick icon beside the text
+             # Display inline text with tick icon using Markdown and HTML
+        st.markdown(
+            f'<p>✅<strong>SCAN IS AN X-RAY. Scanning for pneumonia...</strong></p>',
+            unsafe_allow_html=True
+        )
+            #tick_icon = Image.open(tick_icon_path)
+            #st.image(tick_icon, caption=" ", width=20)  # Display tick icon beside the text
         else:
             st.write("X-RAY SCAN NOT WELL TAKEN. PLEASE SELECT ANOTHER ID.")
 else:
