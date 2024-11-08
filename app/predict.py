f9199a2fdd07f0fb7ba1f19e21340db5eb2b20ad
Promisekel <101825563+Promisekel@users.noreply.github.com>
2024-11-08 10:32:09 +0000

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -1,4 +1,5 @@
 from fastai.vision.all import PILImage
+from PIL import Image as PILImage
 
 def predict(model, img_path):
     img = PILImage.create(img_path)
@@ -7,8 +8,18 @@ def predict(model, img_path):
 
 
 def is_xray(model, img_path):
-    img = PILImage.create(img_path)
+    img = PILImage.open(img_path)
     pred_class, pred_idx, probs = model.predict(img)
-    # Check if the predicted class is in the Xray classes
+    
+    # Define X-ray classes
     xray_classes = ["PNEUMONIA", "NORMAL"]
+    
+    # Check if the predicted class is in the X-ray classes
+    if pred_class in xray_classes:
+        # Determine color based on predicted class
+        color = "red" if pred_class == "NORMAL" else "green"
+        # Display the outcome with the respective color
+        st.markdown(f"<p style='color:{color};'>Outcome of scan: {pred_class}</p>", unsafe_allow_html=True)
+        
     return pred_class in xray_classes
+
