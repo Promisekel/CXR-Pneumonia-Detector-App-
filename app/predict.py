55d11dfdec681f2a7e1ebfbd70695d05cd1062e1
Promisekel <101825563+Promisekel@users.noreply.github.com>
2024-11-08 11:02:13 +0000

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -7,9 +7,9 @@ def predict(model, img_path):
 
 
 # Check if the image is an X-ray and classify for pneumonia
-if is_xray(xray_detector, image_path):
-    st.write("Scanning for pneumonia...")
-    label = predict(model, image_path)
+def is_xray(model, img_path):
+    img = PILImage.create(img_path)
+    pred_class, pred_idx, probs = model.predict(img)
     color = "green" if label == "PNEUMONIA" else "red"
     st.markdown(f"<p style='color:{color}; font-size:20px;'>Outcome of scan: {label}</p>", unsafe_allow_html=True)
 else:
