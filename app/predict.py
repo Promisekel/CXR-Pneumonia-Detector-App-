930f00a109d636e12fa912e6bb3c1ccf3bc5a1d3
Promisekel <101825563+Promisekel@users.noreply.github.com>
2024-11-08 10:39:34 +0000

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -1,22 +1,14 @@
 from fastai.vision.all import PILImage
-import streamlit as st
 
 def predict(model, img_path):
     img = PILImage.create(img_path)
     pred_class, pred_idx, probs = model.predict(img)
     return pred_class
 
+
 def is_xray(model, img_path):
     img = PILImage.create(img_path)
     pred_class, pred_idx, probs = model.predict(img)
-    
-    # Check if the predicted class is in the X-ray classes
+    # Check if the predicted class is in the Xray classes
     xray_classes = ["PNEUMONIA", "NORMAL"]
-    if pred_class in xray_classes:
-        # Set color based on the prediction
-        color = "green" if pred_class == "PNEUMONIA" else "red"
-        # Display the outcome with the respective color
-        st.markdown(f"<p style='color:{color}; font-size:20px;'>Outcome of scan: {pred_class}</p>", unsafe_allow_html=True)
-    
     return pred_class in xray_classes
-
