4ca6d977a10f92c6f429ff0be7dd9dcc857618da
Promisekel <101825563+Promisekel@users.noreply.github.com>
2024-11-09 03:23:07 +0000

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -10,14 +10,5 @@ def is_xray(model, img_path):
     img = PILImage.create(img_path)
     pred_class, pred_idx, probs = model.predict(img)
     # Check if the predicted class is in the Xray classes
-    predicted_class = "PNEUMONIA"  # or "NORMAL"
     xray_classes = ["PNEUMONIA", "NORMAL"]
-    if predicted_class in xray_classes:
-    if predicted_class == "PNEUMONIA":
-        # Display text in red for pneumonia
-        st.markdown(f"<h3 style='color:red;'>{predicted_class}</h3>", unsafe_allow_html=True)
-    else:
-        # Display text in green for normal
-        st.markdown(f"<h3 style='color:green;'>{predicted_class}</h3>", unsafe_allow_html=True)
-
     return pred_class in xray_classes
